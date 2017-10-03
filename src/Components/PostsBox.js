dc6dc377b3edc6c9df43e039e8dc299aeb78e639
dsschapira <dsschapira@gmail.com>
2017-10-03 00:44:08 -0500

    Enabled all category mappings for testing purposes.

--- src/Components/PostsBox.js
+++ src/Components/PostsBox.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import DataActions from '../flux/actions/DataActions.js';
 import DataStore from '../flux/stores/DataStore.js';
 
+//!!!!! Will need to update this before ready for production!!!!
 function catMap(catPath){
     let categories = {
+        'uncategorized':1,
         'player-analysis':3,
         'prospects':4,
-        'roster-construction':5
+        'roster-construction':5,
+        'trade-analysis':6
     };
     return categories[catPath];
 }
+//NON_PRODUCTION_READY_BLOCK^^^
 
 export default class PostsBox extends Component{
 
@@ -44,6 +48,8 @@ export default class PostsBox extends Component{
     }
 
     render(){
+        console.log(this.state.data);
+        console.log(this.props.path);
         return(
             <div>
             </div>
